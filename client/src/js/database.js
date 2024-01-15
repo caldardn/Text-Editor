1ae4b78f86fdbee161f92ebcfad3f842e683d542
caldardn <caldardn@gmail.com>
2024-01-15 12:47:21 -0500

    Adding database code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({content: content});
+    const result = await request;
+    console.log('putDb result: ', result);
+  }catch(err){
+    console.log('putDb error: ', err);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+try{
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result;
+}catch(err){
+  console.log('getDb error: ', err);
+};
+};
 
 initdb();
