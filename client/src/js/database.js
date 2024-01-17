0ae08b882352e76e7eeb370c17e53f015f0fa15e
caldardn <caldardn@gmail.com>
2024-01-17 16:20:55 -0500

    updating

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 2, {
+  openDB('jate', 3, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
