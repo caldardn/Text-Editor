d3ed98ba9ba9328306efd32e1a88fd571e3e9859
caldardn <caldardn@gmail.com>
2024-01-17 15:29:54 -0500

    adding finished code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate', 2, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -12,32 +12,33 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//accepts some content and adds it to the database
 export const putDb = async (content) => {
   try {
-    const db = await openDB('jate', 1);
-    const tx = db.transaction('jate', 'readwrite');
+    console.log('PUT to the db');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
-    const request = store.put({content: content});
+    const request = store.put({ content: content });
     const result = await request;
-    console.log('putDb result: ', result);
-  }catch(err){
-    console.log('putDb error: ', err);
+    console.log('Data saved to the db', result);
+  } catch (err) {
+    console.error('Something went wrong', err);
   }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-try{
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  return result;
-}catch(err){
-  console.log('getDb error: ', err);
-};
+// gets all the content from the database
+export const getDb = async (id, content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+  } catch (err) {
+    console.error('Something went wrong', err);
+  }
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,16 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -20,46 +18,59 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Jate',
+        title: 'J.A.T.E.',
+        favicon: './favicon.ico',
       }),
-
+      new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'sw.js',
+        swDest: 'service-worker.js',
       }),
-      
       new WebpackPwaManifest({
-        name: "Jate",
-        short_name: "Text Editor",
-        description: "A simple text editor",
-        start_url: "./",
-        theme_color: "#31a9e2",
-        background_color: "#272822",
-
+        filename: 'manifest.json',
+        inject: true,
+        fingerprints: false,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Take notes with JasvaScript syntax highlighting!',
+        start_url: './',
+        publicPath: './',
+        purpose: 'maskable',
+        theme_color: '#225ca3',
+        background_color: '#225ca3',
+        orientation: 'portrait',
+        display: 'standalone',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          }
+            src: path.resolve(__dirname, './src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // specify the sizes you want
+            destination: path.join('assets', 'icons'), // specify the output directory
+          },
+        ],
+        prefer_related_applications: true,
+        related_applications: [
+          {
+            platform: 'webapp',
+            url: 'https://text-editor-5000-f2edb74933f9.herokuapp.com/',
+          },
         ],
       }),
     ],
-
     module: {
       rules: [
+        // Add CSS loaders and babel to webpack.
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
+        { test: /\.(png|svg|jpg|jpeg|gif|ico)$/i, type: 'asset/resource' },
         {
           test: /\.m?js$/,
-          exclude: /node_modules/,
+          exclude: /(node_modules|bower_components)/,
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
             },
           },
         },
